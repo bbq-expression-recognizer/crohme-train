d26beca00da1a6cb4e2237a536ac754366023600
ChanMin Kim <kcm1700@gmail.com>
2015-05-30 06:23:11 +0900

    move images into directory

--- run_script.sh
+++ run_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python main.py image.png 2>/dev/null
-python main.py image2.png 2>/dev/null
-python main.py image3.png 2>/dev/null
-python main.py overlap.png 2>/dev/null
-python main.py photo.jpg 2>/dev/null
-python main.py photo2.jpg 2>/dev/null
-python main.py photo3.jpg 2>/dev/null
-python main.py photo4.jpg 2>/dev/null
-python main.py rotated.png 2>/dev/null
-python main.py sgm.png 2>/dev/null
-python main.py wook.png 2>/dev/null
+python main.py images/image.png 2>/dev/null
+python main.py images/image2.png 2>/dev/null
+python main.py images/image3.png 2>/dev/null
+python main.py images/overlap.png 2>/dev/null
+python main.py images/photo.jpg 2>/dev/null
+python main.py images/photo2.jpg 2>/dev/null
+python main.py images/photo3.jpg 2>/dev/null
+python main.py images/photo4.jpg 2>/dev/null
+python main.py images/rotated.png 2>/dev/null
+python main.py images/sgm.png 2>/dev/null
+python main.py images/wook.png 2>/dev/null
 
