596370dc83ec89e36cc33730fc1794a6eabe652e
HeeHoon Kim <csehydrogen@gmail.com>
2015-06-12 02:10:47 +0900

    expression version for quantative analysis

---
+++ inkml2img_expr.py
@@ -0,0 +1,143 @@
+from PIL import Image
+import math
+from scipy import ndimage
+import numpy as np
+import xml.etree.ElementTree as ET
+from os import listdir
+from os.path import isfile, join
+from tex2label import Mapper
+
+import sys
+
+inkmlpath = 'test'
+imgpath = 'test_expr'
+
+# from argument
+if len(sys.argv) == 3:
+    inkmlpath = sys.argv[1]
+    imgpath = sys.argv[2]
+
+print "from: ", inkmlpath
+print "to: ", imgpath
+
+listfile = open(join(imgpath, 'listfile.txt'), 'w')
+img_x = 28*2
+img_y = 64
+margin = 4*2
+density = 32*2
+final_width = 28
+final_height = 64
+mapper = Mapper('texsyms.txt')
+
+files = [f for f in listdir(inkmlpath) if isfile(join(inkmlpath, f))]
+for file_index, f in enumerate(files):
+    if not f.endswith('.inkml'):
+        continue
+    print 'Processing', f, '...', ('(%d / %d)' % (file_index, len(files) ))
+
+    # find traces and trace groups
+    tree = ET.parse(join(inkmlpath, f))
+    root = tree.getroot()
+    traces = []
+    traceGroup = None
+    for child in root:
+        if child.tag.endswith('trace'):
+            traces.append(child)
+        elif child.tag.endswith('traceGroup'):
+            traceGroup = child
+
+    # convert trace elements to coordinate lists
+    coord = []
+    for trace in traces:
+        coord.append([[float(a) for a in a.split()] for a in trace.text.split(',')])
+
+    # group traces and get symbol
+    sym = []
+    tex = []
+    for child in traceGroup:
+        if child.tag.endswith('traceGroup'):
+            tmp = []
+            for child in child:
+                if child.tag.endswith('traceView'):
+                    tmp.append(int(child.get('traceDataRef')))
+                if child.tag.endswith('annotation'):
+                    tex.append(child.text)
+            if len(tmp) > 0:
+                sym.append(tmp)
+
+    sym = [[b for a in sym for b in a]]
+
+    for child in root:
+        if child.tag.endswith('annotation'):
+            if child.get('type') == 'truth':
+                gt = child.text.replace(' ', '').strip('$')
+
+    if '^' in gt:
+        continue
+    if 'frac' in gt:
+        continue
+    if '_' in gt:
+        continue
+
+    for i in xrange(len(sym)):
+        flag = False
+        for a in tex:
+            if mapper.tex2label(a) is None:
+                flag = True
+                break
+        if flag:
+            break
+        traceref = sym[i]
+        min_x = max_x = coord[sym[i][0]][0][0]
+        min_y = max_y = coord[sym[i][0]][0][1]
+        for j in traceref:
+            for x, y in coord[j]:
+                min_x = min(min_x, x)
+                max_x = max(max_x, x)
+                min_y = min(min_y, y)
+                max_y = max(max_y, y)
+        # pass one point trace
+        if min_x == max_x and min_y == max_y:
+            continue
+        # prevent divide-by-zero
+        if min_x == max_x:
+            max_x = max_x + 1
+        if min_y == max_y:
+            max_y = max_y + 1
+        img_x = int(round(img_y / (max_y - min_y) * (max_x - min_x)))
+        final_width = int(round(final_height / (max_y - min_y) * (max_x - min_x)))
+        x_scale = img_x / (max_x - min_x)
+        y_scale = img_y / (max_y - min_y)
+        if x_scale > y_scale:
+            mx = (max_x + min_x) / 2
+            dx = (max_x - min_x) / 2
+            max_x = mx + dx / y_scale * x_scale
+            min_x = mx - dx / y_scale * x_scale
+        else:
+            my = (max_y + min_y) / 2
+            dy = (max_y - min_y) / 2
+            max_y = my + dy / x_scale * y_scale
+            min_y = my - dy / x_scale * y_scale
+
+        sval=[1,2,3,5]
+        for strength in xrange(0, 4):
+            px = np.full((img_y, img_x), 255, dtype=np.uint8)
+            for j in traceref:
+                for k in xrange(1, len(coord[j])):
+                    prevx = coord[j][k - 1][0]
+                    prevy = coord[j][k - 1][1]
+                    nextx = coord[j][k][0]
+                    nexty = coord[j][k][1]
+                    density = int(round(math.hypot(prevx - nextx, prevy - nexty)*2*(img_x-2*margin)/(max_x-min_x)))
+                    for t in xrange(density):
+                        x = prevx + (nextx - prevx) * t / density
+                        y = prevy + (nexty - prevy) * t / density
+                        mx = (x - min_x) / (max_x - min_x) * (img_x - 2 * margin) + margin
+                        my = (y - min_y) / (max_y - min_y) * (img_y - 2 * margin) + margin
+                        px[round(my), round(mx)] = 0
+
+            img = Image.fromarray(ndimage.grey_erosion(px, size=(sval[strength],sval[strength])))
+            imgname = f.rstrip('.inkml') + '_%d_%d.png' % (i, strength)
+            img.resize((final_width, final_height),Image.BICUBIC).save(join(imgpath, imgname))
+            listfile.write(imgname + ' ' + gt + '\n')
+listfile.close()
---
+++ main_expr.py
@@ -0,0 +1,378 @@
+#-*- coding: utf-8 -*-
+import numpy as np
+import math
+from PIL import Image
+from PIL import ImageOps
+from scipy import ndimage
+import os
+import sys
+
+caffe_root = '../caffe/'
+sys.path.insert(0, os.path.join(caffe_root, 'python'))
+import caffe
+
+import binarization
+
+# transforms given 2d grayscale image to height, width
+def transform_input(image, height, width):
+    img_x = width
+    img_y = height
+    margin = 4
+
+    # crop white row padding from image
+    non_white_row_indices = np.where(np.any(image < 128, axis=1))[0]
+    if non_white_row_indices.size == 0:
+        return None, None
+    y1 = np.min(non_white_row_indices)
+    y2 = np.max(non_white_row_indices)+1
+    image = image[y1:y2, :]
+
+
+    # crop white column padding from image
+    non_white_col_indices = np.where(np.any(image < 128, axis=0))[0]
+    if non_white_col_indices.size == 0:
+        return None, None
+    x1 = np.min(non_white_col_indices)
+    x2 = np.max(non_white_col_indices)+1
+    image = image[:, x1:x2]
+
+    resized = Image.fromarray(image,mode='L')
+    resized.thumbnail((img_x-2*margin, img_y-2*margin),Image.ANTIALIAS)
+    result = np.full([img_y,img_x], 255.0, dtype=np.float32)
+    resized_size = resized.size
+
+    rowbeg = margin
+    rowend = height-margin
+    if rowend-rowbeg > resized_size[1]:
+        dec = rowend-rowbeg-resized_size[1]
+        rowbeg = rowbeg + dec/2
+        rowend = rowend - (dec - dec/2)
+
+    colbeg = margin
+    colend = width-margin
+    if colend-colbeg > resized_size[0]:
+        dec = colend-colbeg-resized_size[0]
+        colbeg = colbeg + dec/2
+        colend = colend - (dec - dec/2)
+
+    result[rowbeg:rowend, colbeg:colend] = resized
+    result[result<200] = 0
+    return (y1, y2, x1, x2), result/255.0
+
+
+def parse_expression(IMAGE, net):
+    # Overall pipeline
+    # 1. convert image to binary image.
+    # 2. get best rotation angle (which makes minimum height deviation) and rotate.
+    # 3. divide images with vertical lines
+    # 4. classify each region
+    # 5. use Dynamic Programming approach to get the most likely formula.
+    # 6. back track dynamic programming table.
+
+    net_height, net_width = net.image_dims[0], net.image_dims[1]
+
+    #PIL image
+    gray_image = Image.open(IMAGE).convert('L')
+
+    #numpy 2d array (0~255)
+    binary_array = binarization.to_binary(gray_image)
+
+    # rotate to horizontal line
+    rotated_angle = binarization.rotated_angle(binary_array)
+    binary_array = binarization.rotate_image(binary_array, rotated_angle)
+    # Image.fromarray(binary_array, 'L').save('rotate_normalized.png')
+
+    height, width = binary_array.shape
+
+    # labeling with flood-fill (8-direction)
+    labeled_array, num_features = ndimage.measurements.label(np.invert(binary_array),
+            structure = ndimage.morphology.generate_binary_structure(2, 2))
+    features = []
+    for i in xrange(1, num_features + 1):
+        x_coords = (labeled_array == i).nonzero()[1]
+        xmin = min(x_coords)
+        xmax = max(x_coords)
+        # (start x coordinate, end x coordinate, feature number)
+        features.append([min(x_coords), max(x_coords), i])
+    # sort by starting coordinate
+    features.sort()
+
+    if len(features) == 0:
+        print 'no symbols detected'
+        return '', None
+
+    # virtual symbol at start (for easy dp)
+    merged_features = [[-1, -1, 0]]
+    for i in xrange(num_features):
+        # no overlapping
+        if merged_features[-1][1] < features[i][0]:
+            merged_features.append(features[i])
+        # overlap
+        else:
+            overlap = float(min(merged_features[-1][1], features[i][1]) + 1 - features[i][0])
+            ratio0 = overlap / (merged_features[-1][1] - merged_features[-1][0] + 1)
+            ratio1 = overlap / (features[i][1] - features[i][0] + 1)
+            # > 50% overlap from both features' perspective
+            if ratio0 > 0.5 or ratio1 > 0.5:
+                # merge coords
+                merged_features[-1][1] = max(merged_features[-1][1], features[i][1])
+                # change label
+                labeled_array[labeled_array == features[i][2]] = merged_features[-1][2]
+            else:
+                merged_features.append(features[i])
+
+    # swap to merged one
+    features = merged_features
+    m = len(features)
+    #print features
+
+    # weight[i][j][d]: [i, j] 위치의 d번 symbol의 prediction value.
+    weights = [[[] for _ in xrange(0, m)] for _ in xrange(0, m)]
+
+    # space[i][j]: [i,j] 위치의 space prediction value. 흰색이면 1이다.
+    spaceval = [[0 for _ in xrange(0, m)] for _ in xrange(0, m)]
+
+    layouts = [[(0,0) for _ in xrange(0,m)] for _ in xrange(0,m)]
+
+    # calculate bottom line and top line
+    black_cells = np.where(binary_array == 0)[0]
+    top = int(np.percentile(black_cells, 5))
+    bottom = int(np.percentile(black_cells, 95))
+
+    # number of symbols in neural net
+    trained_symbol_size = 24
+
+    middle_heights = []
+    # fill weights
+    for i in xrange(1, m):
+        caffe_in = np.zeros([m, 1, net_height, net_width], dtype=np.float32)
+        indices = []
+        # we need image buffer, since we cannot directly cut image due to overlap
+        imgbuf = np.full([height, width], 255, dtype = np.uint8)
+        for j in xrange(i, min(i + 1, m)):
+            # fill current feature
+            imgbuf[labeled_array == features[j][2]] = 0
+            layout, transformed = transform_input(
+                imgbuf[max(top - (bottom-top), 0):min(bottom + (bottom-top), len(imgbuf)), features[i][0] : features[j][1] + 1],
+                net_height,
+                net_width)
+            if transformed is None:
+                continue
+            layouts[i][j] = layout
+            if max(layout[1] - layout[0], layout[3] - layout[2]) < (bottom-top) / 4:
+                if (layout[0] + layout[1]) >= (bottom * 3 + top) / 2:
+                    weights[i][j] = [0] * trained_symbol_size + [0, 1]
+                else:
+                    weights[i][j] = [0] * trained_symbol_size + [1, 0]
+                continue
+            middle_heights.append((layout[0] + layout[1]-1)/2)
+            caffe_in[len(indices)][0] = transformed
+            indices.append(j)
+            #img = Image.fromarray(transformed*255,mode='F')
+            #img = img.convert('L')
+            #img.save('test%d-%d.png'%(i,j))
+        out = net.forward_all(**{net.inputs[0]: caffe_in[0:len(indices)]})
+        predictions = out[net.outputs[0]]
+
+        for j, pred in zip(indices,predictions):
+            weights[i][j] = list(pred) + [0, 0] # dot probability
+
+    if len(middle_heights) == 0:
+        raise Exception('no symbols found')
+    middle_height = np.mean(middle_heights)
+
+    # fill spaces probability
+    for i in xrange(1, m):
+        imgbuf = np.full([height, width], 255, dtype = np.uint8)
+        for j in xrange(i, m):
+            imgbuf[labeled_array == features[j][2]] = 0
+            spaceval[i][j] = pow(np.prod(np.mean(imgbuf[top:bottom+1, features[i][0] : features[j][1] + 1], axis=0) / 255.0), 2)
+
+    #0 (
+    #1 )
+    #2 +
+    #3 -
+    #4 /
+    #5 0
+    #6 1
+    #7 2
+    #8 3
+    #9 4
+    #10 5
+    #11 6
+    #12 7
+    #13 8
+    #14 9
+    #15 =
+    #16 [
+    #17 \div
+    #18 \pi
+    #19 \times
+    #20 \{
+    #21 \}
+    #22 ]
+    #23 e
+    #24 \cdot
+    #25 .
+
+
+    syms=[ "(", ")", "+", "-", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "=", "[", "\\div", "\\pi", "\\times", "\\{", "\\}", "]", "e", "\\cdot", "."]
+    sym2kind=[ 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 1, 2, 2, 2, 0, 1, 0]
+
+
+    sigma = len(syms)
+
+    open_symbol={0,16,20}
+    closing_symbol={1,22,21}
+    digits_symbol={5,6,7,8,9,10,11,12,13,14,18,23,25}
+    operator_symbol={2,3,4,15,17,19,24}
+
+    # check if syms[s1] to syms[s2] is possible
+    def possible_transition(s1,s2):
+        if (syms[s1] == "\\pi") and (s2 >= 5 and s2 <= 14):
+            return False
+        if (syms[s1] == "e") and (s2 >= 5 and s2 <= 14):
+            return False
+        if (s1 in operator_symbol) and (s2 in operator_symbol) and (syms[s2] != "-"):
+            return False
+        if (s1 in operator_symbol) and (s2 in closing_symbol):
+            return False
+        if (s1 in open_symbol) and (s2 in operator_symbol) and (syms[s2] != "-"):
+            return False
+        return True
+
+    # dp[i][open][kind]: i번째 위치까지. open된 parenthesis 개수, 마지막 symbol 번호
+    dp = [[[0 for _ in xrange(0, sigma)] for _ in xrange(0,m)] for _ in xrange(0,m)]
+    # for backtracking
+    back = [[[(-1, -1, -1, -1) for _ in xrange(0, sigma)] for _ in xrange(0,m)] for _ in xrange(0,m)]
+
+    for x in range(0, sigma):
+        dp[0][0][x] = 1
+
+    for i in range(0,m):
+        for depth in range(0,i+1):
+            for kind in range(0, sigma):
+                if dp[i][depth][kind] == 0:
+                    continue
+                for j in range(i+1, min(i+3, m)):
+                    newval = dp[i][depth][kind] * spaceval[i + 1][j]
+                    if dp[j][depth][kind] < newval:
+                        dp[j][depth][kind] = newval
+                        back[j][depth][kind] = (i, depth, kind, -1)
+
+                    if len(weights[i + 1][j]) != sigma:
+                        continue
+                    for symbol in range(0,sigma):
+                        if symbol in open_symbol:
+                            next_depth = depth + 1
+                        elif symbol in closing_symbol:
+                            next_depth = depth - 1
+                            if next_depth < 0:
+                                continue
+                        else:
+                            next_depth = depth
+
+                        if not possible_transition(kind, symbol):
+                            continue
+                        newval = dp[i][depth][kind] * weights[i+1][j][symbol]
+                        if dp[j][next_depth][symbol] < newval:
+                            dp[j][next_depth][symbol] = newval
+                            back[j][next_depth][symbol] = (i, depth, kind, symbol)
+
+    def follow_dp():
+        i = m-1
+        depth = 0
+        kind = np.argmax(dp[m-1][0])
+
+        res = []
+
+        while i >= 0:
+            i, depth, kind, symbol = back[i][depth][kind]
+            if symbol >= 0:
+                if symbol in open_symbol:
+                    res.append('(')
+                elif symbol in closing_symbol:
+                    res.append(')')
+                else:
+                    res.append(syms[symbol])
+
+        space_separated = " ".join(res[::-1])
+        return space_separated.replace(". ", ".")
+
+
+    return follow_dp(), Image.fromarray(binary_array, 'L')
+
+# inputs
+MODEL='model.prototxt'
+TRAINED='trained.caffemodel'
+
+if len(sys.argv) == 1:
+    print ("For single image usage: python main.py (image-file-name)")
+    print "running in service mode..."
+
+    # caffe setting
+    caffe.set_mode_cpu()
+    net = caffe.Classifier(MODEL, TRAINED)
+
+    import redis
+
+    # setup redis
+    redis_server = redis.StrictRedis(host='localhost', port=6379, db=2, decode_responses = True)
+    pubsub = redis_server.pubsub()
+
+    pubsub.subscribe(u"request")
+
+    # wait for message
+    for msg in pubsub.listen():
+        print "received", msg
+        if msg['type'] != 'message':
+            continue
+        if msg['data'] != u"image":
+            print "unknown message"
+            continue
+
+        image_id, image_path = redis_server.lpop(u"image").decode('utf-8').split(u',', 1)
+
+        # call function
+        try:
+            result, normalized_image = parse_expression(image_path, net)
+
+            normalized_image.save(image_path + u"_normalized.png")
+            redis_server.rpush(u"result" + unicode(image_id), unicode(result))
+        except:
+            print "failed processing image"
+            redis_server.rpush(u"result" + unicode(image_id), u"")
+
+    pubusb.unsubscribe(u"request")
+    exit(0)
+
+
+
+if len(sys.argv) != 2:
+    print ("Usage: python main.py (image-file-name)")
+    exit(1)
+imgpath=sys.argv[1]
+
+if not os.path.isfile(TRAINED):
+    print ("file not found: %s" % TRAINED)
+    exit(1)
+if not os.path.isfile(MODEL):
+    print ("file not found: %s" % MODEL)
+    exit(1)
+
+# caffe setting
+caffe.set_mode_cpu()
+net = caffe.Classifier(MODEL, TRAINED)
+
+f = open(imgpath + 'listfile.txt')
+r = open(imgpath + 'result.txt', 'w')
+while True:
+    line = f.readline()
+    print line
+    if not line:
+        break
+    filename, ans = line.split()
+    expr, _ =  parse_expression(imgpath + filename, net)
+    expr = expr.replace(' ', '')
+    r.write('%s %s %s\n' % (filename, ans, expr))
+    print ans, expr
